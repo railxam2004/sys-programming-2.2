59e87e6531386a92c4d7fb66328aad9b2c222ad6
railxam2004 <railxam70@gmail.com>
2025-09-26 13:50:12 +0300

    Update №2.py

--- №2.py
+++ №2.py
@@ -1,15 +1,16 @@
-import os
+from pathlib import Path
 
 def dir_size(path):
-    size = 0
-    for name in os.listdir(path):
-        full_path = os.path.join(path, name)
-        if os.path.isfile(full_path):  # если файл
-            size += os.path.getsize(full_path)  # добавляем размер
-        elif os.path.isdir(full_path):  # если папка
-            size += dir_size(full_path)  # рекурсивно суммируем размер
-    return size
+    total_size = 0
+    base_path = Path(path)
+
+    for item in base_path.rglob('*'):  # рекурсивно ищем
+        if item.is_file():  # если это файл
+            total_size += item.stat().st_size  # добавляем размер
+
+    return total_size
 
 path = input("Введите путь к папке: ")
 size = dir_size(path)
+
 print(f"Общий размер папки '{path}': {size}")
